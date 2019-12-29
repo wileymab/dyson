703599479e44519e90bcc9ff5b51c8a051dae163
Matt Wiley <wileymab@gmail.com>
2019-12-28 22:42:49 -0500

    Removed all switches as they reduce portability.

--- aliases/docker.aliases.sh
+++
@@ -1,3 +0,0 @@
-alias swagger_editor='docker run -dit --rm -p 80:8080 swaggerapi/swagger-editor'
-alias docker_stop_all='docker stop $(docker ps -aq) && docker rm $(docker ps -aq)'
-alias ubuntu='docker run -e http_proxy=${HTTP_PROXY} -it ubuntu:18.04'
