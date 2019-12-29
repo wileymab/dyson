ae7b8d1312cc39d6c1b70c62319b05872c3918fa
Matt Wiley <wileymab@gmail.com>
2019-12-28 22:48:12 -0500

    Remove wksp references.

--- functions/bashrc.functions.sh
+++ functions/bashrc.functions.sh
@@ -3,9 +3,6 @@ function setupGlobalFunctions {
 	source $scriptDir/function.setup_prompt.sh
 	source $scriptDir/function.mkcd.sh
 	source $scriptDir/function.todo.sh
-	source $scriptDir/function.wksp.sh
-    source $scriptDir/function.wksp_install.sh
-    source $scriptDir/function.wksp_init.sh
 	source $scriptDir/function.fullpath.sh
 	unset -f setupGlobalFunctions
 }
