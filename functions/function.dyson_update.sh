3e80afe6f8e2d04e9db3d010381791a422005674
Matt Wiley <wileymab@gmail.com>
2019-12-30 17:59:52 -0500

    Added check in dyson_update to check for dyson installation first.

--- functions/function.dyson_update.sh
+++ functions/function.dyson_update.sh
@@ -1,6 +1,11 @@
 function dyson_update {
+    dyson_home="~/.dyson"
     back=$(pwd)
-    cd ~/.dyson
-    git pull origin master
-    cd ${back}
+    if [[ -d "${dyson_home}" ]]; then
+        cd ${dyson_home}
+        git pull origin master
+        cd ${back}
+    else
+        echo "Dyson does not appear to be installed."
+    fi
 }
--- scripts/git.aliases.sh
+++ scripts/git.aliases.sh
@@ -2,4 +2,4 @@ git config --global alias.st  'status -s'
 git config --global alias.co  checkout
 git config --global alias.b   branch
 git config --global alias.br  'branch -r'
-git config --global alias.lo 'log --oneline'
+git config --global alias.lo  'log --oneline'
