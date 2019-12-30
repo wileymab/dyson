a0b9b4d1b75adc550a3dfc2a2fdc38c93c36dbcc
Matt Wiley <wileymab@gmail.com>
2019-12-29 20:25:15 -0500

    Removed the parse_directory nonsense. Fixed bug in prompt setup.

--- functions/function.setup_prompt.sh
+++ functions/function.setup_prompt.sh
@@ -3,8 +3,7 @@ function setup_prompt {
 	source $scriptDir/function.parse_git_dirty.sh
     source $scriptDir/function.parse_zone.sh
 	source $(which shml)
-	dir="$(fgcolor lightgray)$(echo "$PWD")$(fgcolor end)"
-	export PS1="${dir}$(fgcolor cyan)\`parse_git_branch\`$(fgcolor end)\n\`parse_zone\`$(fgcolor lightmagenta)>$(fgcolor end) "
+	export PS1="\n$(fgcolor lightgray)$(echo \`pwd\`)$(fgcolor end)$(fgcolor cyan)\`parse_git_branch\`$(fgcolor end)\n\`parse_zone\`$(fgcolor lightmagenta)>$(fgcolor end) "
 }
 setup_prompt
 unset -f setup_prompt
