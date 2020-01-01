5cc582aae057d24caa27b81821b78a030018aaf7
Matt Wiley <wileymab@gmail.com>
2020-01-01 18:46:30 -0500

    Added git author setup to git setup script.

--- scripts/git.setup.sh
+++ scripts/git.setup.sh
@@ -1,3 +1,12 @@
+# git global author
+echo -n "Git Author Full Name: "
+read author_full_name
+git config --global user.name "${author_full_name}"
+
+echo -n "Git Author Email: "
+read author_email
+git config --global user.email "${author_email}"
+
 # global ignore
 global_ignore=~/.gitignore_global
 git config --global core.excludesfile "${global_ignore}"
