ae6a26bb7ceea32ef15279316ae934856f36eb85
Matt Wiley <wileymab@gmail.com>
2020-01-10 09:33:56 -0500

    Update function.dyson_update.sh

--- functions/function.dyson_update.sh
+++ functions/function.dyson_update.sh
@@ -3,7 +3,7 @@ function dyson_update {
     back=$(pwd)
     if [[ -d "${dyson_home}" ]]; then
         cd ${dyson_home}
-        git pull origin master
+        git co  -- . && git pull origin master
         cd ${back}
     else
         echo "Dyson does not appear to be installed."
